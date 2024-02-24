08953954db19127c60cc2cfef052f3ceddfdeefb
gjemiolo-git <jemiolo.g@gmail.com>
2024-02-24 12:51:06 +0000

    Readd the repo

---
+++ PathFinder/Algorithm.cs
@@ -0,0 +1,111 @@
+﻿// Option A
+// 40506637
+// Gregory Jemiolo
+
+public interface DijkstraAlgorithm
+{
+    public struct Vertex
+    {
+        public int x = 0;
+        public int y = 0;
+
+        public Vertex(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vertex(string x, string y)
+        {
+            this.x = Int32.Parse(x);
+            this.y = Int32.Parse(y);
+        }
+
+        public override string ToString()
+        {
+            string v = "(" + this.x + "," + this.y + ")";
+            return v;
+        }
+    }
+
+    public static double GetEuclidanDistance(Vertex a, Vertex b)
+    {
+        double xResult = Math.Pow(b.x - a.x, 2);
+        double yResult = Math.Pow(b.y - a.y, 2);
+
+        return Math.Sqrt(xResult + yResult);
+    }
+
+    // C# Implementation of Dijkstra's algorithm
+    public static List<int> GetOptimalPath(double[,] graph, int src, int dest)
+    {
+        int vertexCount = graph.GetLength(0);
+        double[] distance = new double[vertexCount];
+        bool[] shortestPath = new bool[vertexCount];
+        int[] previous = new int[vertexCount];          // previous node in optimal path
+
+        // Initialise values of distance and shortestPath
+        for (int i = 0; i < vertexCount; i++)
+        {
+            distance[i] = double.MaxValue;
+            shortestPath[i] = false;
+        }
+        distance[src] = 0;
+
+        for (int count = 0; count < vertexCount - 1; count++)
+        {
+            int u = SelectClosestVertex(distance, shortestPath);
+            shortestPath[u] = true;
+
+            if (u == dest) break; // Destination vertex is reached
+
+            for (int v = 0; v < vertexCount; v++)
+            {
+                if (!shortestPath[v]             // Check if node 
+                    && Convert.ToBoolean(graph[v, u])
+                    && distance[u] != double.MaxValue
+                    && distance[u] + graph[v, u] < distance[v])
+                {
+                    previous[v] = u;
+                    distance[v] = distance[u] + graph[v, u];
+                }
+            }
+        }
+
+        // Build the shortest path using the 'previous' array
+        List<int> path = new List<int>();
+        int step = dest;                    // Follow path back to the source
+        double testDist = Math.Round(distance[step], 2);
+
+        if (distance[step] == double.MaxValue)
+        {
+            // If the destination is unreachable, clear the path and return an empty path.
+            path.Clear();
+            return path;
+        }
+
+        while (step != src)
+        {
+            path.Insert(0, step);
+            step = previous[step];
+        }
+        path.Insert(0, src);
+
+        //Console.WriteLine(testDist);
+        return path;
+    }
+    private static int SelectClosestVertex(double[] distance, bool[] shortestPathTreeSet)
+    {
+        double min = double.MaxValue;   // Setting min value
+        int minIndex = -1;              // Setting dummy index
+
+        for (int v = 0; v < distance.Length; v++)                           // Looping over vertices
+            if (shortestPathTreeSet[v] == false && distance[v] <= min)      // If vertex v is the shortest, and is not included in the path then update the current shorteset
+            {
+                min = distance[v];
+                minIndex = v;
+            }
+
+        return minIndex;    // Return index of the new closest verterx
+    }
+}
---
+++ PathFinder/PathFinder.cs
@@ -0,0 +1,106 @@
+﻿// Option A
+// 40506637
+// Gregory Jemiolo
+class PathFinder
+{
+    static int source = 0;
+    static int vertexCount = 0;
+
+    static void Main(string[] args)
+    {
+        PathFinder shell = new PathFinder();
+
+        if (args.Length < 1 || !File.Exists(args[0] + ".cav"))
+        {
+            Console.WriteLine("Please provide a valid file name.");
+            return;
+        }
+
+        string fileName = args[0];
+        double[,]? graph = ReadFile(fileName + ".cav");
+        List<int> path = DijkstraAlgorithm.GetOptimalPath(graph, source, vertexCount - 1);
+
+        if (path.Count == 0)
+        {
+            string line = "0";
+            File.WriteAllText($"{fileName}.csn", line);
+            //Console.WriteLine("0");
+        }
+        else if (path.Count > 0)
+        {
+            string line = String.Join(" ", path.Select(x => x + 1));
+            File.WriteAllText($"{fileName}.csn", line);
+            //Console.WriteLine(line);
+        }
+    }
+
+    public static double[,] ReadFile(string fileName)
+    {
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("Specified file does not exist.");
+            return new double[0, 0];
+        }
+
+        ushort[]? nums = null;
+        double[,]? graph = null;
+
+        int graphStartIndex = 0;
+        using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+        using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+        using (StreamReader streamReader = new StreamReader(bufferedStream))
+        {
+            int currentArrayIndex = 0;
+            int currentNumberBeingRead = 0;
+            int currentCharAsInt;
+
+            while ((currentCharAsInt = streamReader.Read()) != -1)
+            {
+                if (currentCharAsInt == ',') // Comma for the next number
+                {
+                    if (nums == null) // If it's the first number in the file
+                    {
+                        vertexCount = currentNumberBeingRead;
+                        graphStartIndex = 1 + vertexCount * 2;
+
+                        nums = new ushort[graphStartIndex + vertexCount * vertexCount]; // Initialize the array based on vertexCount
+                        graph = new double[vertexCount, vertexCount];
+                    }
+                    nums[currentArrayIndex++] = (ushort)currentNumberBeingRead; // Add the currently read number to the array
+                    currentNumberBeingRead = 0; // Reset the currentNumberBeingRead to the next number
+                }
+                else
+                {
+                    // Collect digits to get a number, from R to L, subtract 48 as digits start from 48 in ASCI
+                    currentNumberBeingRead = currentNumberBeingRead * 10 + (currentCharAsInt - 48);
+                }
+            }
+            // Add the last number after finishing the loop, as it doesnt have a coma
+            nums[currentArrayIndex] = (ushort)currentNumberBeingRead;
+        }
+
+        // Assign vertices for Euclidan Calculations
+        DijkstraAlgorithm.Vertex[] vertices = new DijkstraAlgorithm.Vertex[vertexCount];
+        for (int i = 0, j = 1; i < vertexCount; i++)
+        {
+            vertices[i] = new DijkstraAlgorithm.Vertex(nums[j++], nums[j++]);
+        }
+
+        // Create Graph and Assign distance
+        for (int i = 0; i < vertexCount; i++)
+        {
+            for (int j = 0; j < vertexCount; j++, graphStartIndex++)
+            {
+                if (nums[graphStartIndex] == 1)
+                {
+                    graph[i, j] = DijkstraAlgorithm.GetEuclidanDistance(vertices[i], vertices[j]);
+                }
+                else
+                {
+                    graph[i, j] = 0d;
+                }
+            }
+        }
+        return graph;
+    }
+}
